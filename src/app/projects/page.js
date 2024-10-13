d953d5a5c57ae74aebe0429e23a10d82f2d4dee3
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-10-13 12:39:01 -0700

    get data from db and pass it as props to a child component

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,5 +1,17 @@
 import ProjectsPage from "@/templates/ProjectsPage";
+import Projects from "@/models/Projects";
+import connectDB from "@/utils/connectDB";
 
-export default function Projects() {
-  return <ProjectsPage />;
+export default async function OpenSourceProjects() {
+  try {
+    await connectDB();
+    const projects = await Projects.find({});
+    return <ProjectsPage projects={projects} />;
+  } catch (error) {
+    return (
+      <p className="text-center italic text-red-500 tracking-wider font-semibold text-2xl">
+        The something wrong is happen...
+      </p>
+    );
+  }
 }
