1d9acda6dace499b093f6c2e98536e25d9d5d1b4
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-10-13 11:11:16 -0700

    make constant social medias

---
+++ src/components/constant/SocialMedias.js
@@ -0,0 +1,21 @@
+import call from "@/Icons/call.svg";
+import telegram from "@/Icons/telegram.svg";
+import github from "@/Icons/github.svg";
+import linkedin from "@/Icons/linkedin.svg";
+
+const SocialMedias = [
+  { link: "tel:+989021571617", icon: call, title: "Call ME" },
+  {
+    link: "https://www.linkedin.com/in/falahati-mahdi/",
+    icon: linkedin,
+    title: "Linkedin",
+  },
+  {
+    link: "https://telegram.me/Mahdi_Falahatii",
+    icon: telegram,
+    title: "Telegram",
+  },
+  { link: "https://github.com/Mahdi-Falahati", icon: github, title: "Github" },
+];
+
+export { SocialMedias };
