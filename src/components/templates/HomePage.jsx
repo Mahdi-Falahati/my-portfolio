9d90a6fc9222b366e135d0a5849772ac9a671024
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-10-13 10:28:57 -0700

    use skills components

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -1,11 +1,13 @@
 import AboutMe from "@/modules/AboutMe";
 import MainContent from "@/modules/MainContent";
+import Skills from "@/modules/Skills";
 
 export default function HomePage({ data }) {
   return (
     <>
       <MainContent />
       <AboutMe educations={data} />
+      <Skills />
     </>
   );
 }
