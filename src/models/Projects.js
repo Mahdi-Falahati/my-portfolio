5005cae55bff33b00a07a5c931d6a2be374714fe
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-10-13 12:38:27 -0700

    make projects model for access to db

---
+++ src/models/Projects.js
@@ -0,0 +1,12 @@
+import { Schema, model, models } from "mongoose";
+
+const projectsSchema = new Schema({
+  title: String,
+  description: String,
+  link: { live: String, code: String },
+  tecnologhy: [String],
+});
+
+const Projects = models.Projects || model("Projects", projectsSchema);
+
+export default Projects;
