021233f1487147feb93a010b55936b340685d6be
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-10-14 06:07:53 -0700

    fixed static path

--- src/utils/fonts.js
+++ src/utils/fonts.js
@@ -3,13 +3,12 @@ import localFont from "next/font/local";
 export const PTSansNarrow = localFont({
   src: [
     {
-      path: "../../public/fonts/PTSansNarrow-Regular.ttf",
+      path: "../../public/Fonts/PTSansNarrow-Regular.ttf",
       weight: "300",
       style: "normal",
     },
-
     {
-      path: "../../public/fonts/PTSansNarrow-Bold.ttf",
+      path: "../../public/Fonts/PTSansNarrow-Bold.ttf",
       weight: "600",
       style: "bold",
     },
